77468dd9fc3c6ecdad37c06d73b89930b1e4183c
dracangelo <kelvinmbuguaw@gmail.com>
2020-02-08 12:13:13 +0300

    craeted a signup function and html

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.landing,name='landing'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def landing(request):
+   
+
     return render(request,'index.html')
+
+
+def signup(request):
+    form = UserCreateForm()
+
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = User.objects.get(username=request.POST['username'])
+            profile = UserProfile(user=user)
+            profile.save()
+
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect('index')
+
+    return render(request, 'signup.html')
